73140f700a485ae911b4d7e050935f062fe2730f
vishal7997 <40819447+vishal7997@users.noreply.github.com>
2024-01-16 13:58:37 +0530

    Refs in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import Component1 from "./Components/Component1";
+// import Component1 from "./Components/Component1";
+import RefsDemo from "./Components/RefsDemo";
 
 function App() {
   return (
     <React.Fragment>
-      <h1>This is App Component</h1>
-      <Component1></Component1>
+      {/* <h1>This is App Component</h1>
+      <Component1></Component1> */}
+      <RefsDemo></RefsDemo>
     </React.Fragment>
   );
 }
