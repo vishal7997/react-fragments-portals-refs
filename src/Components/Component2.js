bc86e0a01029ecf3f427d54228773a504359a328
vishal7997 <40819447+vishal7997@users.noreply.github.com>
2024-01-16 12:55:36 +0530

    Portals in React

--- src/Components/Component2.js
+++ src/Components/Component2.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import PortalComponent from "./PortalComponent";
+import Modal from "./Modal";
 
 function Component2() {
+  let [showModal, updateShowModal] = useState(false);
+  function displayModal() {
+    updateShowModal(true);
+  }
+  function hideModal() {
+    updateShowModal(false);
+  }
   return (
-    <React.Fragment>
+    <div style={{width: 300, position: 'relative'}}>
       <h3>This is Component 2</h3>
-    </React.Fragment>
+      <p>This is a paragraph</p>
+      {/* <PortalComponent></PortalComponent> */}
+      <button onClick={displayModal}>Show Modal</button>
+      <Modal showModal={showModal} hideModal={hideModal}></Modal>
+    </div>
   );
 }
 
---
+++ src/Components/Modal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Modal.css";
+
+function Modal(props) {
+  return ReactDOM.createPortal (
+    <>
+      <div className={`alert ${props.showModal ? "" : "hidden"}`}>
+        <h2>This is an Alert!</h2>
+        <p>
+          This is a very simple example of how a custom alert window can be
+          created and displayed dynamically using Javascript.
+        </p>
+        <button className="modal-btn" onClick={props.hideModal}>
+          OK
+        </button>
+      </div>
+      <div className={`overlay ${props.showModal ? "" : "hidden"}`}></div>
+    </>
+  , document.getElementById('root-portal'));
+}
+
+export default Modal;
